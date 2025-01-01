4652a3540804c786e52c39f2c928a386cb1ca7b7
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-01 08:40:29 +0100

    j'ai termin la function de create

--- classes/Crud.php
+++ classes/Crud.php
@@ -2,5 +2,13 @@
 require 'Connexion.php'
 class Crud 
 {
-    
+    Connexion::connect();
+      static function create ($table,$data){
+        $columns= implode('?,',array_keys($data));
+        $values= implode('?,',array_fill(0,count($data),'?'));
+        $sql="INSERT INTO $table ($columns) values ($values)";
+        $stmt = Connexion :: $conn->prepare($sql);
+        $stmt-> execute(array_values($data))
+      }
+
 }
